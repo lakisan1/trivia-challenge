70de815b7ab953abdd02f89c9897107eea41dbd5
Brian <brian.mcgonagill@tylertech.com>
2017-01-30 18:49:19 -0600

    Added Main Screen after Login
    Create or Join game screen.
    2 large buttons with hover and active coloring. 
    
    Added snackbar as well.  Need to test if I can pass the snackbar message and color in the function call.

---
+++ client/generalLayout/snackbar/snackbar.js
@@ -0,0 +1,12 @@
+showSnackbar = function(message, color) {
+    console.log("Snackbar Called!");
+    // var snackbarText = Session.get("snackbarText");
+    // var notificationColor = Session.get("snackbarColor");
+    var snackbarNotification = document.getElementById("snackbar");
+    snackbarNotification.innerHTML = message;
+    snackbarNotification.style.backgroundColor = color;
+    snackbarNotification.className = "show";
+    setTimeout(function() {
+        snackbarNotification.className = snackbarNotification.className.replace("show", "");
+    }, 5000);
+}
