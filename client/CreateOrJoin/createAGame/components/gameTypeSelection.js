303033fda151154525dd431cd2b5086f5f4b5f55
bmcgonag <bmcgonag@gmail.com>
2017-01-31 16:09:43 -0600

    Hid fields for Creating a Game on initial Render
    Need to add smarts to show and hide based on user selection next.

--- client/CreateOrJoin/createAGame/components/gameTypeSelection.js
+++ client/CreateOrJoin/createAGame/components/gameTypeSelection.js
@@ -1,7 +1,14 @@
+Template.gameTypeSelection.onRendered(function() {
+    $("#secondsQuestion").hide();
+    $("#minutesGame").hide();
+    $("#timeLimit").hide();
+    $("#timeLimitType").hide();
+});
+
 Template.gameTypeSelection.helpers({
 
 });
 
 Template.gameTypeSelection.events({
-    
+
 });
