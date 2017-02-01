1dabab56b59fd21b7e7b1f0b494e9e53ce979c2f
Brian <brian.mcgonagill@tylertech.com>
2017-01-31 19:57:02 -0600

    Added more for the Create Game view.

--- client/CreateOrJoin/createAGame/components/gameTypeSelection.js
+++ client/CreateOrJoin/createAGame/components/gameTypeSelection.js
@@ -10,5 +10,46 @@ Template.gameTypeSelection.helpers({
 });
 
 Template.gameTypeSelection.events({
+    'change #gameType' (event) {
+        event.preventDefault();
 
+        var gameType = $("#gameType").val();
+
+        switch (gameType) {
+            case "timed":
+                $("#minutesGame").show();
+                $("#timeLimit").show();
+                $("#timeLimitType").hide();
+                $("#secondsQuestion").hide();
+                break;
+            case "allA":
+                $("#minutesGame").hide();
+                $("#timeLimit").hide();
+                $("#timeLimitType").hide();
+                $("#secondsQuestion").hide();
+                break;
+            case "allATimed":
+                $("#minutesGame").hide();
+                $("#timeLimit").show();
+                $("#timeLimitType").show();
+                $("#secondsQuestion").hide();
+                break;
+        }
+    },
+    'change #timeLimitType' (event) {
+        event.preventDefault();
+
+        var timeLimitType = $("#timeLimitType").val();
+
+        switch (timeLimitType) {
+            case "timedGame":
+                $("#minutesGame").show();
+                $("#secondsQuestion").hide();
+                break;
+            case "timedQuestions":
+                $("#minutesGame").hide();
+                $("#secondsQuestion").show();
+                break;
+        }
+    }
 });
