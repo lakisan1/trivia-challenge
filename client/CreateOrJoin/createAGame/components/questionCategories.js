e92c9a9102008bbf02393e8a82dd47aad2af264f
bmcgonag <bmcgonag@gmail.com>
2017-02-02 12:34:07 -0600

    Getting the question categories to fill from categories collection.

---
+++ client/CreateOrJoin/createAGame/components/questionCategories.js
@@ -0,0 +1,11 @@
+import { Categories } from '../../../../imports/api/categories.js';
+
+Template.questionCategories.onCreated(function() {
+    this.subscribe('categories');
+});
+
+Template.questionCategories.helpers({
+    getCategories: function() {
+        return Categories.find({});
+    },
+});
