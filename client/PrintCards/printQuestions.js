b50d2231cf794bab9f2314126297261627460836
bmcgonag <bmcgonag@gmail.com>
2017-05-28 10:40:49 -0500

    Added ability to print cards with questions and answers

---
+++ client/PrintCards/printQuestions.js
@@ -0,0 +1,15 @@
+import { Questions } from '../../imports/api/questions.js';
+
+Template.printQuestions.onCreated(function() {
+    this.subscribe('questions');
+});
+
+Template.printQuestions.helpers({
+    allQuestions: function() {
+        Session.set("sequence", Math.floor((Math.random() * 4) + 1));
+        return Questions.find({});
+    },
+    sequence: function() {
+        return Session.get("sequence");
+    }
+});
